e840b27dc861e081e06c013532f637cc9199c8f1
keito0tada <keito0tada@gmail.com>
2022-12-29 14:18:11 +0900

    command

--- test.py
+++ test.py
@@ -1,49 +1,105 @@
 import sys
+import typing
+import re
+from dataclasses import dataclass
+import lark.exceptions
 from lark import Lark
 from lark import Transformer
 from functools import reduce
 
+
+class DuplicatedArgumentError(lark.exceptions.LarkError):
+    pass
+
+
+@dataclass
+class Arg:
+    name: str
+    required: bool
+
+
 class CommandParser:
+    class CommandTransformer(Transformer):
+        def statement(self, tree) -> (list, dict):
+            if len(tree) == 0:
+                return [], {}
+            elif len(tree) == 1:
+                if type(tree[0]) == dict:
+                    return [], tree[0]
+                else:
+                    return tree[0], {}
+            else:
+                return tree[0], tree[1]
+
+        def positionals(self, tree):
+            if len(tree) == 1:
+                return [tree[0]]
+            else:
+                tree[0].append(tree[1])
+                return tree[0]
+
+        def positional(self, tree):
+            return tree[0]
+
+        def optionals(self, tree):
+            if len(tree) == 1:
+                if len(tree[0]) == 1:
+                    return {tree[0][0]: []}
+                else:
+                    return {tree[0][0]: tree[0][1:]}
+            else:
+                if tree[1][0] in tree[0]:
+                    raise DuplicatedArgumentError
+                else:
+                    if len(tree[1]) == 1:
+                        tree[0][tree[1][0]] = []
+                    else:
+                        tree[0][tree[1][0]] = tree[1][1:]
+
+                    return tree[0]
+
+        def optional(self, tree):
+            if len(tree) == 1:
+                return [tree[0]]
+            else:
+                return [i for i in tree]
+
+        def word(self, tree):
+            return tree[0].lower()
+
+        def letter(self, tree):
+            return tree[0].lower()
 
     def __init__(self) -> None:
-        self.argments = []
+        self.arguments: list[Arg] = []
+        with open("test.lark", encoding="utf-8") as grammar:
+            self.parser = Lark(grammar.read())
 
-    def add_argument(self, name):
-        self.argments.append(name)
-        self.__dict__[name] = None
+        self.tree: typing.Union[lark.Tree, None]= None
+        self.result = None
 
-    def find_positional_arg(self):
-        pass
+    def isoptional(self, arg: str):
+        return arg[0] == '-'
 
-    def find_optional_arg(self, arg):
-        pass
+    def add_argument(self, name, required=False):
+        self.arguments.append(self.Arg(name, required))
 
-    def parse_args(self, args):
-        pass
+    def set_arguments(self):
+        index = 0
+        for i in self.arguments:
+            if not self.isoptional(i.name):
+                self.__dict__[i.name] = self.result[0][index]
+                index += 1
+
+        
 
 
+    def parse_args(self, args: str):
+        self.tree = self.parser.parse(args)
+        print(self.tree.pretty())
+        self.result = self.CommandTransformer().transform(self.tree)
+        print(self.result)
 
 
-
-class CommandTransformer(Transformer):
-    def statement(self, tree):
-        return
-
-    def positional(self, tree):
-        return
-
-    def optional(self, tree):
-        return
-
-    def word(self, tree):
-        return tree[0].lower()
-
-    def letter(self, tree):
-        return tree[0].lower()
-
-args = sys.argv
-text = args[1]
-with open("test.lark", encoding="utf-8") as grammar:
-    parser = Lark(grammar.read())
-    tree = parser.parse(text)
-    print(tree.pretty())
+commandparser = CommandParser()
+commandparser.parse_args(sys.argv[1])
